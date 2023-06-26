d36c53458861f3e717f390d0780d7002f31ea3f0
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-26 15:11:37 +0100

    d

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,7 @@
+import Link from 'next/link';
+import styles from './Notes.module.css';
+import CreateNote from './Create';
+
 async function getNotes(){
     const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30');
     const data = await res.json();
