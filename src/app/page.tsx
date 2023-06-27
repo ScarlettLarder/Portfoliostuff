5c01709a1292187ac9412c166581513cd8c0c4f7
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-27 16:51:09 +0100

    iu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ export default function Home() {
   return (
     <main>
      <div className="mt-20 mb-20 ml-5">
-      <h1 className="text-9xl pb-px text-gray-700 lmao font-bold"> Welcome! </h1>
-      <p className="text-6xl font-semibold text-gray-700">You have found my portfolio</p>
+        <h1 className="text-9xl m-2 pb-px text-gray-700 lmao font-bold"> Welcome! </h1>
+        <p className="text-6xl m-2 font-semibold text-gray-600 ">You have found my portfolio</p>
      </div>
-     <p>ffafwaf</p>
+     <div>
+        <Image className='m-6 ' src="/dow.png" alt="dash2" width={50} height={50}/>
+        <Image className='m-6' src="/dow.png" alt="dash2" width={50} height={50}/>
+        <Image className='m-6' src="/dow.png" alt="dash2" width={50} height={50}/>
+     </div>
+     <div>
+     <h1 className="text-8xl mx-10 pb-px text-gray-700 lmao font-bold"> Personal </h1>
+     <h1 className="text-8xl mx-16 pb-px text-gray-700 lmao font-bold"> Statement </h1>
+     <p className="text-4xl mx-16 pb-px text-gray-600 font-bold"> Lorem ipsum dolor sit amet consectetur</p>
+     </div>
     </main>
   )
 }
