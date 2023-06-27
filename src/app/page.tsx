8338d8b3c208111418da965a7973f1a11bf048f3
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-27 16:08:10 +0100

    font to bold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
      <div className="mt-20 mb-20 ml-5">
-      <h1 className="text-9xl pb-px text-gray-700 lmao"> Welcome! </h1>
+      <h1 className="text-9xl pb-px text-gray-700 lmao font-bold"> Welcome! </h1>
       <p className="text-6xl font-semibold text-gray-700">You have found my portfolio</p>
      </div>
      <p>ffafwaf</p>
