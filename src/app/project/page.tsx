9a6def6ffabc4d6376e66e94486ae7a967a25ee5
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-28 16:45:13 +0100

    ggg

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image" 
-import styles from ".page.module.css"
 
 export default function About() {
     return (
         <div> 
-            <h1 className="text-8xl m-2 pb-px ml-10 mt-32 text-gray-700 lmao font-bold flex"> Project's page / </h1>
+            <h1 className="text-8xl m-2 pb-px ml-10 mt-32 text-gray-700 lmao font-bold flex"> Projects page / </h1>
             <h1 className="text-5xl m-2 pb-px ml-10 text-gray-700 font-bold flex"> My past projects on web and more </h1>
         </div>
     )
