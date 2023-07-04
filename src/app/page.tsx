bb54888ef41df43571045879654470b3a1461bbb
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-04 20:29:46 +0100

    edu pop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,19 +40,21 @@ export default function Home() {
          <Image className='ml-10' src="/dow.png" alt="dash2" width={45} height={50}/>
          <Image className='flex float-right ' src="doordash.svg" alt="dash2" width={1000} height={500}/>
       </div>
-      <div>
+
+      <div className="bg-[url('/eduliny2.svg')] bg-no-repeat bg-right bg-contain overflow-visible">
             <h1 className="text-8xl mx-16 pb-px mt-40 text-gray-700 lmao font-bold bg-local"> Education </h1>
             <Image className='flex mx-16 pb-px' src="backtedu.svg" alt="dash2" width={420} height={500}/>
-      </div>
-      <div>
-            <Image className='flex mt-3 float-right' src="eduliny2.svg" alt="dash2" width={450} height={500}/>
+       <div>
             <p className="text-4xl mx-16 pb-px mt-5 text-gray-600 font-bold whitespace-normal"> I am studying T-Level Digital Design, Production and development</p>
             <p className="text-4xl mx-16 pb-px text-gray-600 font-bold whitespace-normal"> at Strode college.</p>
             <p className="text-3xl mx-16 pb-px mt-5 text-gray-600 font-normal whitespace-normal"> The course covers a wide area of digital, the first year being</p>
-            <p className="text-3xl mx-16 pb-px mb-20 text-gray-600 font-normal whitespace-normal"> a general look, and learning python, and the second being web and design.</p>
-            <Image className='flex mt-3' src="eduliny.svg" alt="dash2" width={900} height={500}/>
-            <Image className='m-6' src="/dow.png" alt="dash2" width={45} height={50}/>
+            <p className="text-3xl mx-16 pb-px mb-10 text-gray-600 font-normal whitespace-normal"> a general look, and learning python, and the second being web and design.</p>
+            <Image className="float-right mr-40" src="strode_img.svg" alt="dash2" width={800} height={500}/>
+       </div>
       </div>
+
+      <Image className='flex mt-3' src="eduliny.svg" alt="dash2" width={900} height={500}/>
+      <Image className='m-6' src="/dow.png" alt="dash2" width={45} height={50}/>
       <div className="bg-[url('/Hobbyliny.svg')] bg-no-repeat bg-right bg-contain ">
             <h1 className="text-8xl mx-16 pb-px mt-40 text-gray-700 lmao font-bold bg-local"> Hobbies </h1>
             <p className="text-4xl mx-16 pb-px mt-5 text-gray-600 font-bold whitespace-normal"> I enjoy a variety of things,</p>
