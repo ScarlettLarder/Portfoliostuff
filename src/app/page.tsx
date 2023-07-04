4aa08840a34a2c8f54559474a5c0ccf0dcb7ff47
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-04 20:48:56 +0100

    le done some images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <main>
       <div className="mt-20 mb-20 ml-5">
+        <Image className="float-right mr-40" src="placeholder_top.svg" alt="dash2" width={700} height={500}/>
          <h1 className="text-9xl m-2 pb-px mt-32 text-gray-700 lmao font-bold flex"> Welcome! </h1>
          <p className="text-6xl m-2 font-semibold text-gray-600 ">You have found my portfolio</p>
+        
       </div>
       <div>
          <Image className='m-6 flex' src="/dow.png" alt="dash2" width={50} height={50}/>
@@ -57,12 +59,14 @@ export default function Home() {
       <Image className='m-6' src="/dow.png" alt="dash2" width={45} height={50}/>
       <div className="bg-[url('/Hobbyliny.svg')] bg-no-repeat bg-right bg-contain ">
             <h1 className="text-8xl mx-16 pb-px mt-40 text-gray-700 lmao font-bold bg-local"> Hobbies </h1>
+            <Image className='flex mx-16 pb-px' src="hobby_line.svg" alt="dash2" width={420} height={500}/>
             <p className="text-4xl mx-16 pb-px mt-5 text-gray-600 font-bold whitespace-normal"> I enjoy a variety of things,</p>
-            <p className="text-4xl mx-16 pb-px text-gray-600 font-normal whitespace-normal"> like video making, with my youtube channel that I try to upload on often</p>
-            <p className="text-4xl mx-16 pb-px text-gray-600 font-normal whitespace-normal"> gaming, reading and drawing (not very good at drawing thought)</p>
-            <Image className='m-8 flex' src="/dow.png" alt="dash2" width={45} height={50}/>
-            <Image className='m-8' src="/dow.png" alt="dash2" width={45} height={50}/>
-            <Image className='ml-8 mt' src="/dow.png" alt="dash2" width={45} height={50}/>
+            <p className="text-4xl mx-16 pb-px text-gray-600 font-normal whitespace-normal"> like video making, with my youtube channel that I try to upload on often, i also enjoy</p>
+            <p className="text-4xl mx-16 pb-px text-gray-600 font-normal whitespace-normal"> gaming, reading and drawing (not very good at drawing thought).</p>
+            <Image className="float-right mr-60 mt-5" src="pop_up_hobby.svg" alt="dash2" width={800} height={500}/>
+            <Image className='m-10 flex' src="/dow.png" alt="dash2" width={45} height={50}/>
+            <Image className='m-10' src="/dow.png" alt="dash2" width={45} height={50}/>
+            <Image className='ml-10' src="/dow.png" alt="dash2" width={45} height={50}/>
       </div>
       <div className="bg-[url('/contliny.svg')] bg-no-repeat bg-left bg-contain">
             <h1 className="text-8xl mx-16 pb-px mt-40 text-gray-700 lmao font-bold bg-local"> Contact Me </h1>
@@ -70,13 +74,13 @@ export default function Home() {
             <div>
                 <span className="inline-flex items-center mt-16">
                     <Image className='snap-center snap-right float-right rounded-full ml-10 ' src="mail.svg" alt="dash2" width={40} height={40}/>
-                    <span className="justify-between text-3xl font-normal ml-5">Email - scarlett.insane@gmail.com</span>
+                    <span className="justify-between text-3xl font-normal ml-5"><span className='ml-3 font-semibold '>Email</span> - scarlett.insane@gmail.com</span>
                 </span>
               </div>
               <div>
                 <span className="inline-flex items-center mt-8 mb-80">
                     <Image className='flex snap-center snap-right float-right rounded-full ml-10 ' src="call.svg" alt="dash2" width={40} height={40}/>
-                    <span className="flex justify-between items-center text-3xl font-normal ml-5">Phone number - +44 01278723495</span>
+                    <span className="flex justify-between items-center text-3xl font-normal ml-5"><span className='ml-3 font-semibold mr-3'>Phone number</span> - +44 01278723495</span>
                 </span>
 
             </div>
