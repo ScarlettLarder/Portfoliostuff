40a971cc4b8103578b896091235e959d96648429
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-08-06 19:57:17 +0100

    w

--- src/app/insane.js
+++ src/app/insane.js
@@ -1,37 +1,37 @@
+//"use client"
 // This is for the Welcome! text at the begining to have an animation if hovered.
-"use client"
 
 if (typeof window === "undefined") {
   console.log("Oops, `window` is not defined")
 }
 
-window.onload = function(){ 
+//window.onload = function(){ 
   // List of letters for the code to select from
-  const letters = "Welcomeabcdfghijklmnopqrstuv!";
+//  const letters = "Welcomeabcdfghijklmnopqrstuv!";
 
-  let interval = null;
-  document.querySelector("h1").onmouseover = event => {  
-    let iteration = 0;
+//  let interval = null;
+//  document.querySelector("h1").onmouseover = event => {  
+//    let iteration = 0;
     
-    clearInterval(interval);
+//    clearInterval(interval);
     
-    interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter, index) => {
-          if(index < iteration) {
-            return event.target.dataset.value[index];
-          }
-        
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
+ //   interval = setInterval(() => {
+//      event.target.innerText = event.target.innerText
+//        .split("")
+//        .map((letter, index) => {
+//          if(index < iteration) {
+ //           return event.target.dataset.value[index];
+ //         }
+ //       
+ //         return letters[Math.floor(Math.random() * 26)]
+  //      })
+  //      .join("");
+  //    
+  //    if(iteration >= event.target.dataset.value.length){ 
+  //      clearInterval(interval);
+   //   }
       
-      if(iteration >= event.target.dataset.value.length){ 
-        clearInterval(interval);
-      }
-      
-      iteration += 1 / 3;
-    }, 30);
-  }
-};
+   //   iteration += 1 / 3;
+   // }, 30);
+ // }
+//};
